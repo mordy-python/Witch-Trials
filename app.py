2ac93105e68daf688abe98c795e1bfd3d67ed15b
mordy <imky171@gmail.com>
2021-10-13 19:25:53 -0400

    ok

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mail import Mail, Message
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
@@ -9,20 +10,34 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
 app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
-app.config['MAIL_DEFAULT_SENDER'] = 'isaelwaldner@g.horrycountyschools.net'
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('EMAIL_ADDR')
 mail = Mail(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        recipient = request.form['recipient']
-        msg = Message('Twilio SendGrid Test Email', recipients=[recipient])
-        msg.body = ('Congratulations! You have sent a test email with '
-                    'Twilio SendGrid!')
-        msg.html = ('<h1>Twilio SendGrid Test Email</h1>'
-                    '<p>Congratulations! You have sent a test email with '
-                    '<b>Twilio SendGrid</b>!</p>')
-        mail.send(msg)
-        flash(f'A test message was sent to {recipient}.')
-        return redirect(url_for('index'))
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
+@app.route('/tweets')
+def tweets():
+    tweet_file = open('tweets.json')
+    tweets = json.loads(tweet_file.read())
+    tweet_file.close()
+    twee = []
+    for tweet in tweets:
+        twee.append(tweets[tweet])
+    return render_template('tweets.html', title='Tweets', tweets=twee)
+# TODO: Complete this route and method
+@app.route('/send-confirm', methods=['POST'])
+def send_confirm():
+    recipient = request.form['recipient']
+    msg = Message('Twilio SendGrid Test Email', recipients=[recipient])
+    msg.body = ('Congratulations! You have sent a test email with '
+                'Twilio SendGrid!')
+    msg.html = ('<h1>Twilio SendGrid Test Email</h1>'
+                '<p>Congratulations! You have sent a test email with '
+                '<b>Twilio SendGrid</b>!</p>')
+    mail.send(msg)
+    flash(f'A test message was sent to {recipient}.')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
